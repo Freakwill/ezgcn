b8ddf0268a8757b0e464f3900f96875d573089fc
William Song <30965609+Freakwill@users.noreply.github.com>
2020-05-10 16:23:10 +0800

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+#
+import numpy as np
+from keras import activations, initializers, constraints
+from keras import regularizers
+from keras.engine import Layer
+import keras.backend as K
+from keras.layers import Input, Dropout
+from keras.models import Model
+from keras.optimizers import Adam
+
+from utils import *
+
+
+class GraphConvolution(Layer):
+    """Basic graph convolution layer as in https://arxiv.org/abs/1609.02907"""
+    def __init__(self, units, support=1,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        super(GraphConvolution, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.supports_masking = True
+
+        self.support = support
+        assert support >= 1
+
+    def compute_output_shape(self, input_shapes):
+        features_shape = input_shapes[0]
+        output_shape = (features_shape[0], self.units)
+        return output_shape  # (batch_size, output_dim)
+
+    def build(self, input_shapes):
+        features_shape = input_shapes[0]
+        assert len(features_shape) == 2
+        input_dim = features_shape[1]
+
+        self.kernel = self.add_weight(shape=(input_dim * self.support,
+                                             self.units),
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.units,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.built = True
+
+    def call(self, inputs, mask=None):
+        features = inputs[0]
+        basis = inputs[1:]
+
+        supports = list()
+        for i in range(self.support):
+            supports.append(K.dot(basis[i], features))
+        supports = K.concatenate(supports, axis=1)
+        output = K.dot(supports, self.kernel)
+
+        if self.bias is not None:
+            output += self.bias
+        return self.activation(output)
+
+    def get_config(self):
+        config = {'units': self.units,
+                  'support': self.support,
+                  'activation': activations.serialize(self.activation),
+                  'use_bias': self.use_bias,
+                  'kernel_initializer': initializers.serialize(
+                      self.kernel_initializer),
+                  'bias_initializer': initializers.serialize(
+                      self.bias_initializer),
+                  'kernel_regularizer': regularizers.serialize(
+                      self.kernel_regularizer),
+                  'bias_regularizer': regularizers.serialize(
+                      self.bias_regularizer),
+                  'activity_regularizer': regularizers.serialize(
+                      self.activity_regularizer),
+                  'kernel_constraint': constraints.serialize(
+                      self.kernel_constraint),
+                  'bias_constraint': constraints.serialize(self.bias_constraint)
+        }
+
+        c = super(GraphConvolution, self).get_config() # base config
+        c.update(config)
+        return c
+
+
+from types import SimpleNamespace
+FILTER = SimpleNamespace()
+FILTER.localpool = {'sym_norm':True}
+FILTER.chebyshev = {'max_degree': 2, 'sym_norm':True}
+
+class GCN(Model):
+    def __init__(self, input_dim, output_dim, adj_matrix=None, filter=FILTER.localpool):
+        if filter == FILTER.localpool:
+            # Local pooling filters (see 'renormalization trick' in Kipf & Welling, arXiv 2016)
+            support = 1
+            G = [Input(shape=(None, None), batch_shape=(None, None), sparse=True)]
+
+        elif filter == FILTER.chebyshev:
+            # Chebyshev polynomial basis filters (Defferard et al., NIPS 2016)
+            support = MAX_DEGREE + 1
+            G = [Input(shape=(None, None), batch_shape=(None, None), sparse=True)
+                 for _ in range(support)]
+        else:
+            raise Exception('Invalid filter type.')
+
+        X_in = Input(shape=(input_dim,))
+        H = Dropout(rate=0.5)(X_in)
+        H = GraphConvolution(16, support, activation='relu',
+                             kernel_regularizer=regularizers.l2(5e-4))([H]+G)
+        H = Dropout(rate=0.5)(H)
+        Y = GraphConvolution(output_dim, support, activation='softmax')([H]+G)
+        super(GCN, self).__init__(inputs=[X_in]+G, outputs=Y)
+        self.adj_matrix = adj_matrix
+        self.filter = filter
+
+
+    @classmethod
+    def from_data(cls, X_train, y_train, X_test=None, *args, **kwargs):
+        input_dim = X_train.shape[1]
+        output_dim = y_train.shape[1]
+        model = cls(input_dim, output_dim, *args, **kwargs)
+        if X_test is not None:
+            model.make_graph(X_train, X_test)
+        return model
+
+    def make_graph(self, X_train, X_test, A=None):
+        if A is None:
+            A = self.adj_matrix
+        X = np.vstack((X_train, X_test))
+        if self.filter == FILTER.localpool:
+            # Local pooling filters (see 'renormalization trick' in Kipf & Welling, arXiv 2016)
+            A_ = preprocess_adj(A, self.filter['sym_norm'])
+            self.graph = [X, A_.todense()]
+
+        elif self.filter == FILTER.chebyshev:
+            # Chebyshev polynomial basis filters (Defferard et al., NIPS 2016)
+            L = normalized_laplacian(A, self.filter['sym_norm'])
+            L_scaled = rescale_laplacian(L)
+            T_k = chebyshev_polynomial(L_scaled, self.filter['max_degree'])
+            self.graph = [X]+T_k
+
+        self.adj_size = X.shape[0]
+
+
+    def fit(self, X_train, y_train, X_test=None, val_rate=0.1, *args, **kwargs):
+        if not hasattr(self, 'graph'):
+            if X_test is not None:
+                self.make_graph(X_train, X_test)
+            else:
+                raise Exception('run make_graph method before fitting!')
+
+        NX = X_train.shape[0]
+        NX_ = self.adj_size - NX
+        Ny = y_train.shape[1]
+        mask = random_mask(val_rate, NX)
+        val_mask = np.hstack((mask, np.zeros(NX_, dtype=np.bool)))
+        train_mask = np.hstack((1-mask, np.zeros(NX_, dtype=np.bool)))
+        self.batch_size = self.adj_matrix.shape[0]
+        
+        y_train = np.vstack((y_train, np.zeros((NX_, Ny), dtype=np.bool)))
+        validation_data = (self.graph, y_train, val_mask)
+
+        super(GCN, self).fit(self.graph, y_train, sample_weight=train_mask,
+            validation_data=validation_data, batch_size=self.batch_size,
+            *args, **kwargs)
+
+    def ezfit(self, X, y, X_test):
+        pass
+
+    def valuate(self, X, y):
+        NX_ = X_test.shape[0]
+        test_mask = np.hstack((np.zeros(self.adj_size-NX_), np.ones(NX_)))
+        eval_results = model.evaluate(self.graph, y, sample_weight=test_mask,
+                              batch_size=self.batch_size)
+
+
+    def predict(self, X=None):
+        return super(GCN, self).predict(self.graph, batch_size=self.batch_size)
+
+
---
+++ utils.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+import scipy.sparse as sp
+import numpy as np
+import numpy.linalg as LA
+from sklearn import preprocessing
+from keras.utils import to_categorical
+from scipy.sparse.linalg.eigen.arpack import eigsh, ArpackNoConvergence
+import scipy.stats
+
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+#     # build graph
+#     import networkx as nx
+#     g=nx.read_edgelist("{}{}.cites".format(path, dataset))
+#     N=len(g)
+#     adj=nx.to_numpy_array(g,nodelist=idx_features_labels[:, 0])
+#     adj = sp.coo_matrix(adj)
+
+
+def normalize_adj(adj, symmetric=True):
+    if symmetric:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -0.5).flatten(), 0)
+        a_norm = adj.dot(d).transpose().dot(d).tocsr()
+    else:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -1).flatten(), 0)
+        a_norm = d.dot(adj).tocsr()
+    return a_norm
+
+
+def preprocess_adj(adj, symmetric=True):
+    adj += sp.eye(adj.shape[0])
+    adj = normalize_adj(adj, symmetric)
+    return adj
+
+
+# def sample_mask(idx, l):
+#     mask = np.zeros(l)
+#     mask[idx] = 1
+#     return np.array(mask, dtype=np.bool)
+
+def random_mask(p, l):
+    b = scipy.stats.bernoulli(p)
+    return b.rvs(l).astype(np.bool)
+
+
+# def get_splits(y):
+#     idx_train = range(140)
+#     idx_val = range(200, 500)
+#     idx_test = range(500, 1500)
+    
+#     y_train = np.zeros(y.shape, dtype=np.int32) #y:label
+#     y_val = np.zeros(y.shape, dtype=np.int32)
+#     y_test = np.zeros(y.shape, dtype=np.int32)
+
+#     y_train[idx_train] = y[idx_train]
+#     y_val[idx_val] = y[idx_val]
+#     y_test[idx_test] = y[idx_test]
+
+#     train_mask = sample_mask(idx_train, y.shape[0])
+#     val_mask = sample_mask(idx_val, y.shape[0])
+#     test_mask = sample_mask(idx_test, y.shape[0])
+
+#     return y_train, y_val, y_test, train_mask,val_mask,test_mask
+
+
+def normalized_laplacian(adj, symmetric=True):
+    adj_normalized = normalize_adj(adj, symmetric)
+    laplacian = sp.eye(adj.shape[0]) - adj_normalized
+    return laplacian
+
+
+def rescale_laplacian(laplacian):
+    try:
+        print('Calculating largest eigenvalue of normalized graph Laplacian...')
+        largest_eigval = eigsh(laplacian, 1, which='LM', return_eigenvectors=False)[0]
+    except ArpackNoConvergence:
+        print('Eigenvalue calculation did not converge! Using largest_eigval=2 instead.')
+        largest_eigval = 2
+
+    scaled_laplacian = (2. / largest_eigval) * laplacian - sp.eye(laplacian.shape[0])
+    return scaled_laplacian
+
+
+def chebyshev_polynomial(X, k):
+    """Calculate Chebyshev polynomials up to order k. Return a list of sparse matrices."""
+    print("Calculating Chebyshev polynomials up to order {}...".format(k))
+
+    T_k = [sp.eye(X.shape[0]).tocsr(), X]
+
+    def chebyshev_recurrence(T_k_minus_one, T_k_minus_two, X):
+        X_ = sp.csr_matrix(X, copy=True)
+        return 2 * X_.dot(T_k_minus_one) - T_k_minus_two
+
+    for i in range(2, k+1):
+        T_k.append(chebyshev_recurrence(T_k[-1], T_k[-2], X))
+
+    return T_k
+
+
+# extended by William
+def load_from_csv(train_path="train.csv", test_path="test.csv", normalized=False):
+    X_y = np.genfromtxt(train_path, delimiter=',')
+    X_train = X_y[:, :-1]
+    y_train = encode_onehot(X_y[:, -1])
+    X_y = np.genfromtxt(test_path, delimiter=',')
+    X_test = X_y[:, :-1]
+    y_test = encode_onehot(X_y[:, -1])
+    if normalized:
+        X_train /= X_train.sum(1).reshape(-1, 1)
+        X_test /= X_test.sum(1).reshape(-1, 1)
+    return X_train, X_test, y_train, y_test
+
+
+# similarity: (x1, x2) |-> [0,1]
+# x1==x2 -> 1
+def cosine(x1, x2):
+    return np.dot(x1, x2)/LA.norm(x1)*LA.norm(x2)
+
+def hamming(x1, x2):
+    return np.mean(x1==x2)
+
+def norm(x1, x2, p=2):
+    # 1/(||x1-x2||+1)
+    return 1 / (LA.norm(x1-x2, p)+1)
+
+def make_adj(X_train, X_test, similarity=cosine):
+    X_total = np.vstack((X_train, X_test))
+    edges = []
+    N = X_total.shape[0]
+    for i in range(N):
+        for j in range(i+1, N):
+            if similarity(X_total[i,:], X_total[j,:]) > 0.75:
+                edges.append([i,j])
+    edges = np.array(edges)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(N, N), dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj += adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    return adj
